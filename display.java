37a86629c7f49ff60020146cfad4813a5b51e3d3
ganesh-legend <168000400+ganesh-legend@users.noreply.github.com>
2024-05-05 20:50:26 +0530

    new line added display.java

--- display.java
+++ display.java
@@ -3,5 +3,6 @@ class display
   public static void main(String args[])
   {
     System.out.println("Hello..! This is first java program");
+    System.out.println("This line has been added on Github after push from local machine");
   }
 }
